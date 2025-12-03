028711f2f155150978bbb58fba085a2ccf8b7964
Tim Mendenhall <5558561+timmendenhall@users.noreply.github.com>
2025-12-03 14:56:20 -0500

    Adding day 1 part B tests and config options

--- src/bin/day-1/main.rs
+++ src/bin/day-1/main.rs
@@ -6,17 +6,22 @@ const SAFE_VALUE_MAX: i32 = 100; // Exclusive, 100 = 0-99 range
 
 struct Config {
     file_path: String,
+    should_count_all_0s: bool, // First part doesn't count all 0 clicks, the second part of day 1 does
 }
 
 impl Config {
     fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 2 {
+        if args.len() < 3 {
             return Err("not enough arguments");
         }
 
         let file_path = args[1].clone();
+        let should_count_all_0s = args[2].clone().parse().unwrap();
 
-        Ok(Config { file_path })
+        Ok(Config {
+            file_path,
+            should_count_all_0s,
+        })
     }
 }
 
@@ -39,28 +44,48 @@ fn do_puzzle(config: Config) {
     let mut lines_count = 0;
 
     for line in contents.lines() {
-        safe_value = execute_line(line, safe_value);
+        let (new_safe_value, password_increment) =
+            execute_line(line, safe_value, config.should_count_all_0s);
         lines_count += 1;
-
-        if safe_value == 0 {
-            password += 1;
-        }
+        password += password_increment;
+        safe_value = new_safe_value
     }
 
     println!("Parsed lines: {}", lines_count);
     println!("Password is: {}", password);
 }
 
-fn execute_line(instruction: &str, current_value: i32) -> i32 {
+fn execute_line(instruction: &str, current_value: i32, should_count_all_0s: bool) -> (i32, i32) {
+    let mut result: i32 = 0;
+    let password_increment_by;
+    let mut rotate_by: i32 = 0;
+
     if instruction.starts_with("L") {
         let split: Vec<&str> = instruction.split('L').collect();
-        return rotate_left(current_value, split[1].parse().unwrap());
+        rotate_by = split[1].parse().unwrap();
+        result = rotate_left(current_value, rotate_by);
+        rotate_by *= -1; // Inverting this value as it's added for the password_increment_by calc
     } else if instruction.starts_with("R") {
         let split: Vec<&str> = instruction.split('R').collect();
-        return rotate_right(current_value, split[1].parse().unwrap());
+        rotate_by = split[1].parse().unwrap();
+        result = rotate_right(current_value, rotate_by);
     }
 
-    i32::MAX // Failed to find L/R
+    if should_count_all_0s {
+        let mut rotations = 0;
+
+        if current_value != 0 && current_value + rotate_by <= 0 {
+            rotations += 1
+        }
+
+        rotations += (current_value + rotate_by).abs() / SAFE_VALUE_MAX;
+
+        password_increment_by = rotations;
+    } else {
+        password_increment_by = if result == 0 { 1 } else { 0 };
+    }
+
+    (result, password_increment_by)
 }
 
 fn rotate_left(safe_value: i32, rotate_by: i32) -> i32 {
@@ -114,13 +139,57 @@ mod tests {
 
     #[test]
     fn test_execute_line() {
-        let result = execute_line("L480", 50);
+        let (result, _password_increase_by) = execute_line("L480", 50, false);
         assert_eq!(result, 70);
 
-        let result = execute_line("L48", 0);
+        let (result, _password_increase_by) = execute_line("L48", 0, false);
         assert_eq!(result, 52);
 
-        let result = execute_line("R60", 50);
+        let (result, _password_increase_by) = execute_line("R60", 50, false);
         assert_eq!(result, 10);
+
+        let (result, password_increase_by) = execute_line("L68", 50, true);
+        assert_eq!(result, 82);
+        assert_eq!(password_increase_by, 1);
+
+        let (result, password_increase_by) = execute_line("L30", 82, true);
+        assert_eq!(result, 52);
+        assert_eq!(password_increase_by, 0);
+
+        let (result, password_increase_by) = execute_line("R48", 52, true);
+        assert_eq!(result, 0);
+        assert_eq!(password_increase_by, 1);
+
+        let (result, password_increase_by) = execute_line("R60", 95, true);
+        assert_eq!(result, 55);
+        assert_eq!(password_increase_by, 1);
+
+        let (result, password_increase_by) = execute_line("L55", 55, true);
+        assert_eq!(result, 0);
+        assert_eq!(password_increase_by, 1);
+
+        let (result, password_increase_by) = execute_line("L99", 99, true);
+        assert_eq!(result, 0);
+        assert_eq!(password_increase_by, 1);
+
+        let (result, password_increase_by) = execute_line("L82", 14, true);
+        assert_eq!(result, 32);
+        assert_eq!(password_increase_by, 1);
+
+        let (result, password_increase_by) = execute_line("R548", 52, true);
+        assert_eq!(result, 0);
+        assert_eq!(password_increase_by, 6);
+
+        let (result, password_increase_by) = execute_line("R102", 99, true);
+        assert_eq!(result, 1);
+        assert_eq!(password_increase_by, 2);
+
+        let (result, password_increase_by) = execute_line("R502", 99, true);
+        assert_eq!(result, 1);
+        assert_eq!(password_increase_by, 6);
+
+        let (result, password_increase_by) = execute_line("L55", 55, true);
+        assert_eq!(result, 0);
+        assert_eq!(password_increase_by, 1);
     }
 }
