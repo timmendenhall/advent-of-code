187ddfe47b5b543d11e53ad4257cf67044ed8a98
Tim Mendenhall <5558561+timmendenhall@users.noreply.github.com>
2025-12-02 22:16:48 -0500

    WIP reading file, basic functions

--- src/bin/day-1/main.rs
+++ src/bin/day-1/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::fs;
 
+const SAFE_VALUE_MAX: i32 = 100; // Exclusive, 100 = 0-99 range 
+
 fn main() {
     println!("Hello, world!");
     do_puzzle()
@@ -9,17 +11,35 @@ fn main() {
 fn do_puzzle() {
     let _password = 0;
     let _safe_value = 50;
+
+    let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
+    println!("{}", contents);
 }
 
-fn rotate_right(safe_value: &mut i32, rotate_by: i32) {
-    *safe_value += rotate_by;
+fn execute_line(instruction: &str, current_value: i32) -> i32 {
+    let rotate_by = 12; // TODO: <--
+    if instruction.starts_with("L") {
+        return rotate_left(current_value, rotate_by);
+    }
+    return rotate_right(current_value, rotate_by);
+}
+
+fn rotate_left(safe_value: i32, rotate_by: i32) -> i32 {
+    let mut ret = safe_value - rotate_by;
+    ret % SAFE_VALUE_MAX
+}
+
+fn rotate_right(safe_value: i32, rotate_by: i32) -> i32 {
+    let ret = safe_value + rotate_by;
+    ret % SAFE_VALUE_MAX
 }
 
 #[test]
 fn test_rotate_right() {
-    let mut safe_value = 50;
+    let result = rotate_right(50, 2);
+    assert_eq!(result, 52);
 
-    rotate_right(&mut safe_value, 2);
-
-    assert_eq!(safe_value, 52);
+    // Wraps over max value
+    let result = rotate_right(SAFE_VALUE_MAX, 2);
+    assert_eq!(result, 1);
 }
