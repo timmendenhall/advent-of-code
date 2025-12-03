b4980d966d8f46ca588ad613981c137bcbff5232
Tim Mendenhall <5558561+timmendenhall@users.noreply.github.com>
2025-12-03 10:51:51 -0500

    Adding day 1 code, env arg parsing, and some unit tests

--- src/bin/day-1/main.rs
+++ src/bin/day-1/main.rs
@@ -1,31 +1,70 @@
 use std::env;
 use std::fs;
+use std::process;
 
 const SAFE_VALUE_MAX: i32 = 100; // Exclusive, 100 = 0-99 range 
 
-fn main() {
-    println!("Hello, world!");
-    do_puzzle()
+struct Config {
+    file_path: String,
 }
 
-fn do_puzzle() {
-    let _password = 0;
-    let _safe_value = 50;
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }
 
-    let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
-    println!("{}", contents);
+        let file_path = args[1].clone();
+
+        Ok(Config { file_path })
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    do_puzzle(config)
+}
+
+fn do_puzzle(config: Config) {
+    let mut _password = 0;
+    let mut _safe_value = 50;
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    for line in contents.lines() {
+        _safe_value = execute_line(line, _safe_value);
+
+        if _safe_value == 0 {
+            _password += 1;
+        }
+    }
+
+    println!("Password is: {}", _password);
 }
 
 fn execute_line(instruction: &str, current_value: i32) -> i32 {
-    let rotate_by = 12; // TODO: <--
     if instruction.starts_with("L") {
-        return rotate_left(current_value, rotate_by);
+        let split: Vec<&str> = instruction.split('L').collect();
+        return rotate_left(current_value, split[1].parse().unwrap());
+    } else if instruction.starts_with("R") {
+        let split: Vec<&str> = instruction.split('R').collect();
+        return rotate_right(current_value, split[1].parse().unwrap());
     }
-    return rotate_right(current_value, rotate_by);
+
+    -1 // Failed to find L/R
 }
 
 fn rotate_left(safe_value: i32, rotate_by: i32) -> i32 {
-    let mut ret = safe_value - rotate_by;
+    let ret = safe_value - rotate_by;
+    if ret < 0 {
+        return SAFE_VALUE_MAX - ret.abs() % SAFE_VALUE_MAX;
+    }
     ret % SAFE_VALUE_MAX
 }
 
@@ -34,12 +73,42 @@ fn rotate_right(safe_value: i32, rotate_by: i32) -> i32 {
     ret % SAFE_VALUE_MAX
 }
 
-#[test]
-fn test_rotate_right() {
-    let result = rotate_right(50, 2);
-    assert_eq!(result, 52);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    // Wraps over max value
-    let result = rotate_right(SAFE_VALUE_MAX, 2);
-    assert_eq!(result, 1);
+    #[test]
+    fn test_rotate_right() {
+        let result = rotate_right(50, 2);
+        assert_eq!(result, 52);
+
+        // Wraps correctly
+        let result = rotate_right(SAFE_VALUE_MAX - 1, 1);
+        assert_eq!(result, 0);
+
+        let result = rotate_right(SAFE_VALUE_MAX, 5 + (5 * SAFE_VALUE_MAX));
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn test_rotate_left() {
+        let result = rotate_left(50, 2);
+        assert_eq!(result, 48);
+
+        // Wraps correctly
+        let result = rotate_left(0, 1);
+        assert_eq!(result, SAFE_VALUE_MAX - 1);
+    }
+
+    #[test]
+    fn test_execute_line() {
+        let result = execute_line("L48", 50);
+        assert_eq!(result, 2);
+
+        let result = execute_line("L48", 0);
+        assert_eq!(result, 52);
+
+        let result = execute_line("R60", 50);
+        assert_eq!(result, 10);
+    }
 }
