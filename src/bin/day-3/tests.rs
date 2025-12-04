e4a1ddac0579686562d32d9b1e6c650720b8f83d
Tim Mendenhall <5558561+timmendenhall@users.noreply.github.com>
2025-12-04 12:42:07 -0500

    adding day 3 part b's tests (currently fail)

--- src/bin/day-3/tests.rs
+++ src/bin/day-3/tests.rs
@@ -17,3 +17,18 @@ fn test_part_a_strategy() {
     let result = part_a_strategy("818181911112111");
     assert_eq!(result, 92);
 }
+
+#[test]
+fn test_part_b_strategy() {
+    let result = part_b_strategy("987654321111111");
+    assert_eq!(result, 987654321111);
+
+    let result = part_b_strategy("811111111111119");
+    assert_eq!(result, 811111111119);
+
+    let result = part_b_strategy("234234234234278");
+    assert_eq!(result, 434234234278);
+
+    let result = part_b_strategy("818181911112111");
+    assert_eq!(result, 888911112111);
+}
