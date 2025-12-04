456fb9a08baaff355390b6a94e37f0c3ba936204
Tim Mendenhall <5558561+timmendenhall@users.noreply.github.com>
2025-12-04 10:48:37 -0500

    Checking in some WIP on day 3 part a

---
+++ src/bin/day-3/main.rs
@@ -0,0 +1,84 @@
+use std::env;
+use std::fs;
+use std::process;
+
+struct Config {
+    file_path: String,
+    strategy: fn(&str) -> i64,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let file_path = args[1].clone();
+        let strategy_str = args[2].clone();
+
+        let strategy = match strategy_str.as_str() {
+            "part-a" => part_a_strategy,
+            "part-b" => part_b_strategy,
+            _ => part_a_strategy,
+        };
+
+        Ok(Config {
+            file_path,
+            strategy,
+        })
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    do_puzzle(config)
+}
+
+fn do_puzzle(config: Config) {
+    let mut password = 0;
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    for line in contents.lines() {
+        password += do_password_strategy(line, config.strategy);
+    }
+
+    println!("Password is: {}", password);
+}
+
+fn do_password_strategy(bank_input: &str, strategy: fn(&str) -> i64) -> i64 {
+    strategy(bank_input)
+}
+
+fn part_a_strategy(bank_input: &str) -> i64 {
+    println!("{}", bank_input);
+    let bank_input_length = bank_input.chars().count();
+    let first_digit = bank_input[0..bank_input_length - 2].chars().max().unwrap();
+    let first_digit_index = bank_input.find(first_digit).unwrap();
+    let remaining_bank_input = &bank_input[first_digit_index + 1..];
+    let second_digit = remaining_bank_input.chars().max().unwrap();
+
+    println!(
+        "first digit: {} index: {} --",
+        first_digit, first_digit_index
+    );
+
+    println!("second digit: {}  --", second_digit);
+    println!("reamining -- {}", remaining_bank_input);
+
+    let mut bank_value = String::new();
+    bank_value.push(first_digit);
+    bank_value.push(second_digit);
+
+    bank_value.parse().unwrap()
+}
+
+fn part_b_strategy(_bank_input: &str) -> i64 {
+    0
+}
