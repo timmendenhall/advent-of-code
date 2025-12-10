472eec4224b037b083ec3e2220a1666095561c42
Tim Mendenhall <5558561+timmendenhall@users.noreply.github.com>
2025-12-10 09:43:15 -0500

    WIP on day 8 part a

--- src/bin/day-8/main.rs
+++ src/bin/day-8/main.rs
@@ -6,15 +6,30 @@ use std::process;
 #[cfg(test)]
 mod tests;
 
+#[derive(Clone, Debug, PartialEq)]
 struct Vec3 {
-    x: i32,
-    y: i32,
-    z: i32,
+    x: f32,
+    y: f32,
+    z: f32,
 }
 
 impl Vec3 {
-    fn new(x: i32, y: i32, z: i32) -> Self {
+    fn new(x: f32, y: f32, z: f32) -> Self {
         Self { x, y, z }
+    }
+    fn from(string: &str) -> Self {
+        let split: Vec<&str> = string.split(',').collect();
+        let x = split[0].parse().unwrap();
+        let y = split[1].parse().unwrap();
+        let z = split[2].parse().unwrap();
+        Self { x, y, z }
+    }
+    fn distance_to(self: &Self, vec: &Vec3) -> f32 {
+        let x = (self.x - vec.x).powi(2);
+        let y = (self.y - vec.y).powi(2);
+        let z = (self.z - vec.z).powi(2);
+
+        (x + y + z).sqrt()
     }
 }
 
@@ -42,9 +57,34 @@ fn do_puzzle(config: Config) {
 }
 
 fn do_part_a(contents: String) -> i64 {
+    let all_junction_boxes = build_junction_boxes(contents);
+    let mut available_junction_boxes = all_junction_boxes.clone();
+    let mut circuits: Vec<Vec<Vec3>> = Vec::new();
+
+    for junction_box in all_junction_boxes {
+        for checking_box in available_junction_boxes.iter() {
+            let distance = junction_box.distance_to(checking_box);
+            println!(
+                "Checking {:#?} -> {:#?} | distance = {}",
+                junction_box, checking_box, distance
+            );
+        }
+    }
+
     1
 }
 
 fn do_part_b(contents: String) -> i64 {
     2
+}
+
+fn build_junction_boxes(contents: String) -> Vec<Vec3> {
+    let mut junction_boxes = Vec::new();
+
+    for line in contents.lines() {
+        let junction_box = Vec3::from(line);
+        junction_boxes.push(junction_box);
+    }
+
+    junction_boxes
 }
